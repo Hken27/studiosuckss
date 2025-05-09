d10ee6301f8525900a5815a805192f2b97461e96
mbolang <nicosakauw027@gmail.com>
2025-05-09 15:26:23 +0700

    service beres

--- assets/js/service.js
+++ assets/js/service.js
@@ -1,3 +1,12 @@
+const title = document.getElementById('stickyTitle');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) { // Mulai efek setelah scroll 100px
+        title.classList.add('scaled');
+    } else {
+        title.classList.remove('scaled');
+    }
+});
 document.querySelectorAll(".service-item").forEach(item => {
     const descText = item.getAttribute("data-desc");
     item.querySelector(".desc").textContent = descText;
